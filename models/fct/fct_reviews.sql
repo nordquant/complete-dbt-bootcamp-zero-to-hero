616cbde5998c75885aa9a7759a5337164a4624ce
Zoltan C. Toth <zoltan@zoltanctoth.com>
2023-07-03 07:25:16 +0200

    Update surrogate_key_treat_nulls_as_empty_strings fct_reviews.sql

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -8,7 +8,7 @@ WITH src_reviews AS (
   SELECT * FROM {{ ref('src_reviews') }}
 )
 SELECT 
-  {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+  {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
   *
 FROM src_reviews
 WHERE review_text is not null
