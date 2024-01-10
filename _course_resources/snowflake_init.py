4986c1d7ab9f1343955d2fb3b28855ba30ac4a19
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-01-10 08:16:41 +0100

    deleting dagster, adding snowflake wip plus upgrade to 1.7.1

--- _course_resources/snowflake_init.py
+++ _course_resources/snowflake_init.py
@@ -3,7 +3,7 @@
 #pip install sqlalchemy snowflake-sqlalchemy markdown pyyaml
 
 import streamlit as st
-
+import re
 import sqlalchemy as sa
 from sqlalchemy import create_engine
 from markdown import markdown
@@ -38,43 +38,27 @@ def get_snowflake_connection(account, username, password):
     return connection
 
 
-def read_sql_from_md(file_path):
-    with open(file_path, 'r') as f:
-        md_text = f.read()
-
-    html = markdown(md_text, extensions=[CodeHiliteExtension()])
-    st.write(html)
-    soup = BeautifulSoup(html, 'html.parser')
-
-    code_blocks = soup.find_all('#snowflake_setup')
-    #sql_blocks = [block.text for block in code_blocks if block.get('class') and 'sql' in block.get('class')]
-    st.write(code_blocks)
-    st.write(code_blocks.text)
-    return code_blocks
-
-    return sql_blocks
-
-def execute_sql(connection, sql_blocks):
+def execute_sqls(connection, sql_blocks):
     for sql in sql_blocks:
-        if "-- END OF SNOWFLAKE DATA IMPORT" in sql:
-            break
         st.write(sql)
-        # result = connection.execute(sql)
-        # for row in result:
-        #     st.write(row)
 
 
 def main():
     pw = os.environ.get("SNOWFLAKE_PASSWORD") if os.environ.get("SNOWFLAKE_PASSWORD") else ""
     hostname = st.text_input('Hostname', 'laimquw-pfb79199')
     username = st.text_input('USername', 'admin')
-    password = st.text_input('Password', 'ps')
+    password = st.text_input('Password', pw)
     if st.button("check"):
         st.write(f"megklikkelte {hostname} {username} {password}")
 
-        connection = get_snowflake_connection(hostname, username, password)
+        #connection = get_snowflake_connection(hostname, username, password)
 
-        sql_blocks = read_sql_from_md(CURRENT_DIR + "/course-resources.md")
+        with open(CURRENT_DIR + "/course-resources.md", 'r') as file:
+            md = file.read().rstrip()
+        import_pattern = r'sql {#snowflake_import}(.*?)```'; 
+        match = re.search(import_pattern, md, re.DOTALL);
+        import_sqls = [row.strip() for row in match.group(1).split(';')]
+        st.write(import_sqls)
         #execute_sql(connection, sql_blocks)
 
 if __name__ == '__main__':
