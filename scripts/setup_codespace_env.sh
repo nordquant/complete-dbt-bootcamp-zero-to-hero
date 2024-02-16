476dd162e5d9b46a59a491fcdec8139abf9b9276
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-02-16 10:21:35 +0000

    scripts/setup_codespace_env.sh

--- scripts/setup_codespace_env.sh
+++ scripts/setup_codespace_env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -eu
+set -x
 
 mkdir -p ~/.dbt
 ln -sf $(readlink -f profiles.yml) ~/.dbt/profiles.yml
-code profiles.yml
-
 pip3 install --user -r requirements.txt
+
+code profiles.yml
