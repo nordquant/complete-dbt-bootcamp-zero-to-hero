5dbcf1962c960d283a028b4012ca8c38a6e20e6b
Zoltan C. Toth <zoltan@zoltanctoth.com>
2024-03-04 09:07:58 +0100

    macros update

--- dbtlearn/macros/logging.sql
+++ dbtlearn/macros/logging.sql
@@ -2,5 +2,5 @@
     {{ log("Call your mom!") }}
     {{ log("Call your dad!", info=True) }} --> Logs to the screen, too
 --  {{ log("Call your dad!", info=True) }} --> This will be put to the screen
-    {% log("Call your dad!", info=True) %} --> This won't be executed
+    {# log("Call your dad!", info=True) #} --> This won't be executed
 {% endmacro %}
