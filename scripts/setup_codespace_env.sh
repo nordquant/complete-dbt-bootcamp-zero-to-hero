c584eff365165832a07e79332f5550e83d1849b2
Zoltan C. Toth <zoltan@zoltanctoth.com>
2026-01-09 09:47:28 +0100

    streamline codespace + add python extension

--- scripts/setup_codespace_env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e 
-
-uv sync
-echo 'if [ -f .venv/bin/activate ]; then source .venv/bin/activate; fi' >> ~/.bashrc \
-  && echo 'if [ -f .venv/bin/activate ]; then source .venv/bin/activate; fi' >> ~/.zshrc
