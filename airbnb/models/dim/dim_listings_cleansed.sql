146ee55bbc4698f9c3969617bdc2c1e2ae11021d
Zoltan C. Toth <zoltan@nordquant.com>
2026-05-19 14:24:42 +0200

    prod wip

--- airbnb/models/dim/dim_listings_cleansed.sql
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -23,7 +23,6 @@ SELECT
     10,
     2
   ) AS price,
-  price_str,
   created_at,
   updated_at
 FROM
--- airbnb/models/dim/dim_listings_w_hosts.sql
+++ airbnb/models/dim/dim_listings_w_hosts.sql
@@ -17,7 +17,6 @@ SELECT
     l.room_type,
     l.minimum_nights,
     l.price,
-    l.price_str,
     l.host_id,
     h.host_name,
     h.is_superhost as host_is_superhost,
