cbc6aecc9f170037783abdbdc630acc5084e145d
diepedkpf-crypto <diepe.dkpf@gmail.com>
2026-06-19 12:32:06 +0000

    Correction of some tests

--- airbnb/models/dim/dim_listings_w_host.sql
+++ airbnb/models/dim/dim_listings_w_host.sql
@@ -18,7 +18,7 @@ SELECT
     l.listing_name,
     l.room_type,
     l.minimum_nights,
-    l.price AS price_usd,
+    l.price,
     l.host_id,
     h.host_name,
     h.is_superhost AS host_is_superhost,
